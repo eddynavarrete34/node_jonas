6a28000abdf724190238aac1e50bb3c7930a0fc7
Eddy <eddynavarrete@gmail.com>
2023-12-23 12:16:48 -0600

    first load

---
+++ 4-natours/section06/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const app = express();
+
+// 1) MIDDLEWARE
+if(process.env.NODE_ENV === 'development'){
+	app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+
+// 2) ROUTE HANDLERS
+
+
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+ 
+module.exports = app;
---
+++ 4-natours/section06/controllers/userController.js
@@ -0,0 +1,37 @@
+exports.getAllUsers = (req, res) => {
+	console.log("app.get('/api/v1/users')");
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet define'
+	});
+}
+
+exports.createUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet define'
+	});
+}
+
+exports.getUser = (req, res) => {
+	console.log('getUser');
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet define'
+	});
+}
+
+exports.updateUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet define'
+	});
+}
+
+exports.deleteUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'This route is not yet define'
+	});
+}
+
